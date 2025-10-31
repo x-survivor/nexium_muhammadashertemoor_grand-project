fe9ac24f9f82f05255f4b82394d2861a59b1e376
Muhammad Ashar Temoor <muhammadasher183@gmail.com>
2025-11-01 01:32:48 +0500

    Feat: Improve login page design

--- grand_project/app/(auth)/login/page.jsx
+++ grand_project/app/(auth)/login/page.jsx
@@ -4,6 +4,8 @@ import { supabase } from "@/lib/supabaseClient";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Sparkles, WandSparkles } from "lucide-react";
 
 export default function LoginPage() {
   const [email, setEmail] = useState("");
@@ -18,31 +20,52 @@ export default function LoginPage() {
     }
   };
   return (
-    <section className="w-screen flex justify-center items-center h-[100vh] bg-dark">
-      <div className="w-1/4 flex justify-center items-center p-4 rounded-2xl">
+    <section className="p-6 w-full flex justify-center items-center h-screen bg-black">
+      <div className="w-full lg:w-4/12 flex flex-col justify-center items-start p-6 gap-8 border-2 border-gray-700 rounded-3xl bg-gray-900">
+        <div>
+          <h2 className="text-2xl font-bold text-white">Magic Link</h2>
+          <h3 className="text-xs">
+            Enter your email and we'll send you a secure login link
+          </h3>
+        </div>
         <form
-          className="w-full flex flex-col justify-center items-center gap-4 p-8 border-2 border-gray-700 rounded-lg"
+          className="w-full flex flex-col justify-center gap-4 "
           onSubmit={handleSubmit}
         >
-          <Label
-            className="text-4xl md:text-6xl mb-4 bg-gradient-to-r from-blue-400 via-purple-500 to-blue-600 bg-clip-text text-transparent uppercase"
-            htmlFor="email"
+          <div className="flex flex-col gap-2">
+            <Label className="text-white font-semibold text-xs" htmlFor="email">
+              Email Address
+            </Label>
+            <Input
+              className="w-full"
+              type="email"
+              placeholder="abc@example.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <Button
+            className="w-full bg-linear-to-r from-blue-700 to-purple-600 text-white"
+            type="submit"
           >
-            Login
-          </Label>
-          <Input
-            className="w-full"
-            type="email"
-            placeholder="Enter Your Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-          <Button className="w-full" type="submit">
-            Get The Link
+            <WandSparkles className="w-4 h-4 mr-1" /> Send Magic Link
           </Button>
           {message && <p className="mt-4">{message}</p>}
         </form>
+        <Card className="w-full bg-gray-800 border-2 border-gray-700 text-xs">
+          <CardHeader>
+            <h3 className="font-semibold text-sm flex items-center">
+              {" "}
+              <Sparkles className="w-4 h-4 mr-2" /> What is a Magic Link?
+            </h3>
+          </CardHeader>
+          <CardContent>
+            A magic link is a secure and convenient way to log in without a
+            password. Simply enter your email, and we'll send you a link to
+            access your account.
+          </CardContent>
+        </Card>
       </div>
     </section>
   );
